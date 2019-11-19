3f414f2d5dea761cb1475fe039ea0a512a61d9c0
polinabogun <polinabogun@gmail.com>
2019-11-19 22:20:15 +0200

    Polina Bogun
    homework8

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+
+var codeWord1 = "Wait";
+var codeWord2 = "Snake";
+var codeWord3 = "unsefe";
+var codeWord4 = "Juice ";
+var codeWord5 = "Purple";
+var codeWord6 = "red";
+var codeWord7 = "grey";
+var codeWord8 = "November";
+var codeWord9 = "!"
+
+alert(codeWord1[0] + codeWord2[4] + codeWord4[5] + codeWord2[2] + codeWord5[2] + codeWord2[4] + codeWord4[5] + codeWord3[2] + codeWord1[3] 
+    + codeWord3[0] + codeWord6[2] + codeWord7[3] + codeWord1[2] + codeWord3[1] + codeWord7[1] + codeWord4[5] + codeWord4[0] + codeWord1[1]
+     + codeWord8[2] + codeWord1[1] + codeWord2[0] + codeWord4[3] + codeWord6[0] + codeWord4[2] + codeWord5[3] + codeWord1[3] +codeWord9);
